9ae5b580cb33e2b1ba7e24aba75bdb9c6586fa3f
Muresan Andrei <muresan.1andrei@gmail.com>
2017-09-17 23:31:40 +0300

    add video and video list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCXiNaCPt4eF-3xxg8qmQcWLFVpJ97vW9Y';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: 'skate'}, (videos) => {
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0] 
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} 
+        />
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('.container'));
